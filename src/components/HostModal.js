7be3f496f80cb8b856c8aba9438c33b1e6a00638
khanna01 <govmfkdtm@gmail.com>
2024-08-20 05:25:44 +0900

    궁합페이지 데이터 수정

--- src/components/HostModal.js
+++ src/components/HostModal.js
@@ -3,6 +3,35 @@ import Tarot from '../assets/imgs/Tarot.svg'
 import cancleIcon from '../assets/imgs/Page4/cancleIcon.png'
 
 const HostModal = ({ closeModal }) => {
+  const compatibilityObj = [
+    {
+      title: '성격 궁합',
+      score: 75,
+      description:
+        '라봉이의 적극적이고 리더십 있는 성격과 감귤이의 차분하고 내성적인 성격은 대조적이\n' +
+        '  지만, 보완적으로 작용할 수 있습니다. 서로의 부족한 부분을 메워줄 수 있는 관계로, 성\n' +
+        '  격적 궁합이 상당히 좋은 편입니다.',
+    },
+    {
+      title: '가치관 일치',
+      score: 60,
+      description:
+        '라붕봉이는 외향적이고 도전적이며, 리더 역할을 선호합니다. 반면, 감귤이는 내성적이고 신중하며 깊이 있는 사고를 추구합니다. 이들의 가치관은 다소 차이가 있지만, 목표에 대해 진지한 태도를 공유하고 있어 일치하는 부분이 존재합니다.',
+    },
+    {
+      title: '협력 가능성',
+      score: 80,
+      description:
+        '라봉이는 팀을 이끌며 프로젝트를 추진하는 데 능숙하고, 감귤이는 창의적이면서도 분석적인 사고를 가지고 있어 서로의 능력을 잘 활용할 수 있습니다. 이들의 조합은 큰 시너지를 낼 수 있습니다.',
+    },
+    {
+      title: '의사소통',
+      score: 70,
+      description:
+        '라봉이는 직설적이고 빠르게 의사소통하는 것을 선호하는 반면, 감귤이는 깊이 있는 대화를 즐기지만 천천히 마음을 여는 스타일입니다. 이들이 서로의 의사소통 스타일을 이해하고 존중할 때, 원활한 소통이 가능합니다.',
+    },
+  ]
+
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
       {/* 모달 배경 */}
@@ -43,28 +72,25 @@ const HostModal = ({ closeModal }) => {
         <div className="px-6">
           {/* 성격궁합 등 4개의 평가 */}
           <div className="space-y-4 mt-4 max-h-[calc(100vh-450px)] overflow-y-scroll">
-            {['성격궁합', '가치관 일치', '협력 가능성', '의사소통'].map(
-              (title, index) => (
-                <div key={index} className="bg-[#465290] p-4 rounded-[15px]">
-                  <div className="flex justify-between items-center">
-                    <div className="text-xl flex items-center font-bold">
-                      <span role="img" aria-label="icon" className="mr-2">
-                        ✌️
-                      </span>
-                      {title}
-                    </div>
-                    <div className="text-lg font-bold">SCORE : 75 / 100</div>
+            {compatibilityObj.map((value, index) => (
+              <div key={index} className="bg-[#465290] p-4 rounded-[15px]">
+                <div className="flex justify-between items-center">
+                  <div className="text-xl flex items-center font-bold">
+                    <span role="img" aria-label="icon" className="mr-2">
+                      ✌️
+                    </span>
+                    {value.title}
                   </div>
-                  <p className="mt-2 text-sm leading-relaxed border-t-2 border-[#334188]">
-                    <br />
-                    라붕이의 적극적이고 리더십 있는 성격과 감귤이의 차분하고
-                    내성적인 성격은 대조적이지만, 본연적으로 작용할 수 있습니다.
-                    서로의 부족한 부분을 메워줄 수 있는 관계로, 성격적 궁합이
-                    상당히 좋은 편입니다.
-                  </p>
+                  <div className="text-lg font-bold">
+                    SCORE : {value.score} / 100
+                  </div>
                 </div>
-              ),
-            )}
+                <p className="mt-2 text-sm leading-relaxed border-t-2 border-[#334188]">
+                  <br />
+                  {value.description}
+                </p>
+              </div>
+            ))}
           </div>
         </div>
 
