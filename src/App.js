64669b1cb535155b2e98e0adf7592ba6f396d83d
minsu kim <kimminsu31415@gmail.com>
2024-08-19 15:53:51 +0900

    파일 구조 정리

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import './App.css'
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import PageA from './pages/PageA'
+import PageB from './pages/PageB'
+import PageC from './pages/PageC'
+import CardFlip from './components/CardFlip'
+import ChatPage from './pages/ChatPage'
+import Sidebar from './components/Sidebar'
+import Page1 from './pages/Page1'
+import Page2 from './pages/Page2'
+import Page3 from './pages/Page3'
 
 function App() {
   return (
-    <div>
-      <div>App 화면</div>
-      <div className="font-pretendard font-[900]">프리텐다드 100</div>
+    <div className="App">
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sidebar" element={<Sidebar />} />
+
+        <Route path="/page1" element={<Page1 />} />
+        <Route path="/page2" element={<Page2 />} />
+        <Route path="/page3" element={<Page3 />} />
+
+        <Route path="/pageA" element={<PageA />} />
+        <Route path="/pageB" element={<PageB />} />
+        <Route path="/pageC" element={<PageC />} />
+        <Route path="/chatPage" element={<ChatPage />} />
+        <Route path="/cardFlip" element={<CardFlip />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import Sidebar from '../components/Sidebar'
+import testImage from '../assets/imgs/testImage.png'
+
+const Home = () => {
+  const navigate = useNavigate()
+
+  const selectCharacterGenderPageA = async (gender) => {
+    // 선택된 성별 문자열을 PageA로 전달
+    navigate('/PageA', { state: { gender: gender } })
+  }
+
+  const selectCharacterGenderPageC = async (gender) => {
+    // 선택된 성별 문자열을 PageC로 전달
+    navigate('/PageC', { state: { gender: gender } })
+  }
+
+  return (
+    <div className="flex min-h-screen justify-center relative">
+      {/* 메인 컨텐츠 */}
+      <div className="w-full max-w-[400px] overflow-x-hidden bg-gray-300 ">
+        <div className="px-[24px] py-[40px] flex flex-col gap-4 justify-between bg-slate-400">
+          여기는 Home 페이지
+          <div className="grid grid-cols-1 gap-4">
+            <img src={testImage} alt="testImage" />
+            <button
+              className="bg-blue-300"
+              onClick={() => selectCharacterGenderPageA('female')}
+            >
+              여자 캐릭터 선택 - pageA
+            </button>
+            <button
+              className="bg-blue-100"
+              onClick={() => selectCharacterGenderPageA('male')}
+            >
+              남자 캐릭터 선택 - pageA
+            </button>
+            <button
+              className="bg-pink-300"
+              onClick={() => selectCharacterGenderPageC('female')}
+            >
+              여자 캐릭터 선택(카드 컴포넌트 적용) - PageC
+            </button>
+            <button
+              className="bg-pink-100"
+              onClick={() => selectCharacterGenderPageC('male')}
+            >
+              남자 캐릭터 선택(카드 컴포넌트 적용) - PageC
+            </button>
+            <Link to="/page1" className="bg-yellow-300">
+              Page1로 이동
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
